675ecd1558965789965070e54fc59f5a6fc3a616
juan <juanignaciocasais@gmail.com>
2021-05-29 19:22:11 -0300

    correccion ejercicio

--- etapa1.cpp
+++ etapa1.cpp
@@ -4,53 +4,14 @@ using namespace std;
 
 float pedirDatokm(string);
 
-int pedirVueltas(string);
-
-void pedirDatoVector(int vector[], string mensaje, int dimension)
-{
-    cout << mensaje;
-    cin >> vector[dimension];
-}
-
-void vueltaMasRapida(int vector[], int dimension, int &tiempo, int &posicion)
-{
-    tiempo = vector[0];
-
-    for (int i = 1; i < dimension; i++)
-    {
-        if (vector[i] < tiempo)
-        {
-            tiempo = vector[i];
-            posicion = i;
-        }
-    }
-
-    return;
-}
-
-void vueltaMasLenta(int vector[], int dimension, int &tiempo, int &posicion)
-{
-    tiempo = vector[0];
-
-    for (int i = 1; i < dimension; i++)
-    {
-        if (vector[i] > tiempo)
-        {
-            tiempo = vector[i];
-            posicion = i;
-        }
-    }
-
-    return;
-}
+int pedirDato(string);
 
 float velocidadMedia(float, int, int);
 
 int main()
 {
-
-    int numVuelta[20] = {0};
-    int tiempoVuelta[20] = {0};
+    int numVuelta = 0;
+    int tiempoVuelta = 0;
     int cantidadVueltas = 0;
     float kmCircuito = 0;
     int masRapida = 0;
@@ -60,25 +21,34 @@ int main()
 
     kmCircuito = pedirDatokm("Ingrese los kilómetros del circuito: ");
 
-    cantidadVueltas = pedirVueltas("Ingrese la cantidad de vueltas a registrar (<= 20): ");
+    cantidadVueltas = pedirDato("Ingrese la cantidad de vueltas a registrar (<= 20): ");
 
     for (int i = 0; i < cantidadVueltas; i++)
     {
-        pedirDatoVector(numVuelta, "Ingrese el número de vuelta: ", i);
-        pedirDatoVector(tiempoVuelta, "Ingrese el tiempo de la vuelta (MMSS): ", i);
+        numVuelta = pedirDato("Ingrese el número de vuelta: ");
+
+        tiempoVuelta = pedirDato("Ingrese el tiempo de la vuelta (MMSS): ");
+
+        if (tiempoMasRapida > tiempoVuelta || masRapida == 0)
+        {
+            tiempoMasRapida = tiempoVuelta;
+            masRapida = numVuelta;
+        }
+
+        if (tiempoMasLenta < tiempoVuelta || masLenta == 0)
+        {
+            tiempoMasLenta = tiempoVuelta;
+            masLenta = numVuelta;
+        }
     }
 
-    vueltaMasRapida(tiempoVuelta, cantidadVueltas, tiempoMasRapida, masRapida);
-
-    vueltaMasLenta(tiempoVuelta, cantidadVueltas, tiempoMasLenta, masLenta);
-
-    cout << "La vuelta más rápida fue la nº " << numVuelta[masRapida]
+    cout << "La vuelta más rápida fue la nº " << masRapida
          << " y su tiempo es de " << tiempoMasRapida / 100 << ":" << tiempoMasRapida % 100 << " minutos" << endl;
 
-    cout << "La vuelta más lenta fue la nº " << numVuelta[masLenta]
+    cout << "La vuelta más lenta fue la nº " << masLenta
          << " y su tiempo es de " << tiempoMasLenta / 100 << ":" << tiempoMasLenta % 100 << " minutos" << endl;
 
-    cout << "La velocidad media fue de " << velocidadMedia(kmCircuito, tiempoMasRapida, tiempoMasLenta) << " m/s." << endl;
+    cout << "Porcentaje de reducción de tiempo: " << velocidadMedia(kmCircuito, tiempoMasRapida, tiempoMasLenta) << endl;
 
     return 0;
 }
@@ -91,7 +61,7 @@ float pedirDatokm(string mensaje)
     return numero;
 }
 
-int pedirVueltas(string mensaje)
+int pedirDato(string mensaje)
 {
     int numero;
     cout << mensaje;
@@ -109,14 +79,7 @@ float velocidadMedia(float kmCircuito, int tiempoMasRapida, int tiempoMasLenta)
 
     int tiempoMasLentaSeg = (tiempoMasLenta / 100) * 60 + (tiempoMasLenta % 100);
 
-    if (tiempoMasLentaSeg - tiempoMasRapidaSeg > 0)
-    {
-        velocidad = (float)mtsCircuito / (tiempoMasLentaSeg - tiempoMasRapidaSeg);
-    }
-    else
-    {
-        velocidad = (float)mtsCircuito / tiempoMasRapidaSeg;
-    }
+    velocidad = (float)tiempoMasLentaSeg / tiempoMasRapidaSeg - 1;
 
     return velocidad;
 }
